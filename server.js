d9094211173d1158e7ecee8b42f73c9c00dd7652
aman <aman.jangra@celebal.tech>
2022-12-19 18:51:42 +0530

    check-for-already-login/loggedout user

--- server.js
+++ server.js
@@ -67,6 +67,18 @@ app.delete('/logout', (req, res) => {
     res.redirect('/login')
 })
 
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/login')
+}
 
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+}
 
 app.listen(8000)
